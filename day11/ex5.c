b84b631639d9ffda1113c93b7563044a08778625
tamjung00 <streetlamp0312@gmail.com>
2016-07-13 17:40:09 +0900

    ...

--- day11/ex5.c
+++ day11/ex5.c
@@ -3,8 +3,20 @@
 
 #include "../mapEditor/map.h"
 
-void map_drawTile_trn()
+void map_drawTile_trn(_S_MAP_OBJECT *pObj,int posx,int posy,_S_MAP_OBJECT *pTarget)
 {
+	for(int iy=0;iy<pObj->m_header.m_nHeight;iy++) {
+			for(int ix=0;ix<pObj->m_header.m_nWidth;ix++) {
+				
+				int nPixel = pObj->m_pBuf[iy*pObj->m_header.m_nWidth +ix];
+				if(nPixel != 0) {
+				 	map_PutTile(pTarget,ix + posx,iy + posy,
+					pObj->m_pBuf[iy*pObj->m_header.m_nWidth +ix]);
+			}
+		}
+	
+	}
+
 
 }
 
@@ -28,7 +40,7 @@ int main()
 	map_init(&humanObj);
 	map_load(&humanObj,"human.dat");
 	puts("\r\n-------------------------");
-	map_drawTile(&humanObj,5,5,&screenBuf);
+	map_drawTile_trn(&humanObj,5,5,&screenBuf);
 	map_dump(&screenBuf,Default_Tilepalete);
 
 	
--- mapEditor/editor.c
+++ mapEditor/editor.c
@@ -17,7 +17,9 @@ int main()
 
 	MapObject.m_header.m_nSkima = 1;
 	MapObject.m_pBuf = NULL;
-	char TilePalette[] = {'.','#','@','%'};
+	//char TilePalette[] = {'.','#','@','%'};
+	
+	puts("text TileMap Editor v1.1");
 
 	while(bLoop)
 	{
@@ -32,7 +34,7 @@ int main()
 
 		}
 		else if( !strcmp(pTemp,"dump")) {
-			map_dump(&MapObject,TilePalette);
+			map_dump(&MapObject,Default_Tilepalete);
 		}
 		else if(!strcmp(pTemp,"new")) {
 			//new 8 4
